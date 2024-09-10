0de47be6d038d94c4331ffe5c8f9485736f0f513
seobkun325 <seobkun325@gmail.com>
2024-09-10 09:41:55 +0900

    fix

--- baekjoon/c/hash/7785.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  char name[6] = 'Askar';
-  unsigned long a;
-  sscanf(name, "%1x", &a);
-  printf("%lu", a);
-}
--- baekjoon/c/자료구조/example.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  int b = 3;
-  int* a;
-  a = &b;
-  printf("%d\n", *a);
-}
